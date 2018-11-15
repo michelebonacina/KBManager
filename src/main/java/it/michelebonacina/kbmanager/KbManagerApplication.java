917eba74d78f9f2573ab64544dc022ab29792c88
Michele Bonacina <michele.bonacina@gmail.com>
2018-11-15 14:24:57 +0100

    initial commit

---
+++ src/main/java/it/michelebonacina/kbmanager/KbManagerApplication.java
@@ -0,0 +1,12 @@
+package it.michelebonacina.kbmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbManagerApplication.class, args);
+	}
+}
